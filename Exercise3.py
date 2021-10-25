58391d1a2f2ddcbc44b73fa47526d3fd50b806ee
Ruslan <mephistopooh45@gmail.com>
2021-10-25 21:22:21 +0500

    Home3
    Home3

---
+++ Exercise3.py
@@ -0,0 +1,18 @@
+thesaurus_dict = {}
+
+
+def thesaurus(*saurus):
+    for name in saurus:
+        for upper in name[1:]:
+            if upper.istitle():
+                if upper in thesaurus_dict:
+                    if name[0] in thesaurus_dict[upper]:
+                        thesaurus_dict[upper][name[0]].append(name)
+                    else:
+                        thesaurus_dict[upper][name[0]] = [name]
+                else:
+                    thesaurus_dict[upper] = dict.fromkeys([name[0]], [name])
+
+
+thesaurus("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева", "Анатолий Артемьев")
+print(thesaurus_dict)
