96bccb20de94598a93a6b5b24abb6b4b8cd927c4
Ruslan <mephistopooh45@gmail.com>
2021-10-25 19:40:16 +0500

    Homework #3
    Homework #3

---
+++ exercise_1.py
@@ -0,0 +1,26 @@
+number_dict = {
+    'zero': 'ноль',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять',
+}
+
+
+def num_translate(str_number):
+    if str_number.lower() in number_dict:
+        if str_number.istitle():
+            return number_dict[str_number.lower()].capitalize()
+        else:
+            return number_dict[str_number]
+    else:
+        return None
+
+
+print(num_translate('Five'))
